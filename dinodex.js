5930103e67412f8b543ad35968f645af6eff2de6
Jordan Brown <jmbrown@andrew.cmu.edu>
2015-04-15 23:18:49 -0400

    im tired as fuck

--- dinodex.js
+++ dinodex.js
@@ -2,16 +2,16 @@ Router.map(function(){
 	this.route('keypad', {
 		path: '/'
 	});
-	this.route('dino1');
-  this.route('dino2');
-  this.route('dino3');
-  this.route('dino4');
-  this.route('dino5');
-  this.route('dino6');
-  this.route('dino7');
-  this.route('dino8');
-  this.route('dino9');
-  this.route('dino0');
+	this.route('Triceratops');
+  this.route('Stegosaurus');
+  this.route('Apatosaurus');
+  this.route('Archaeopteryx');
+  this.route('Gojirasaurus');
+  this.route('T_Rex');
+  this.route('Yoshi');
+  this.route('Thesaurus');
+  this.route('Philosoraptor');
+  this.route('Barney');
 });
 if (Meteor.isClient) {
   // counter starts at 0
@@ -27,73 +27,73 @@ if (Meteor.isClient) {
     'click button': function (e) {
       // increment the counter when button is clicked
       var id = e.currentTarget.id;
-      if (id == "1") Router.go('dino1');
-      else if (id == "2") Router.go('dino2');
-      else if (id == "3") Router.go('dino3');
-      else if (id == "4") Router.go('dino4');
-      else if (id == "5") Router.go('dino5');
-      else if (id == "6") Router.go('dino6');
-      else if (id == "7") Router.go('dino7');
-      else if (id == "8") Router.go('dino8');
-      else if (id == "9") Router.go('dino9');
-      else if (id == "0") Router.go('dino0');
+      if (id == "1") Router.go('Triceratops');
+      else if (id == "2") Router.go('Stegosaurus');
+      else if (id == "3") Router.go('Apatosaurus');
+      else if (id == "4") Router.go('Archaeopteryx');
+      else if (id == "5") Router.go('Gojirasaurus');
+      else if (id == "6") Router.go('T_Rex');
+      else if (id == "7") Router.go('Yoshi');
+      else if (id == "8") Router.go('Thesaurus');
+      else if (id == "9") Router.go('Philosoraptor');
+      else if (id == "0") Router.go('Barney');
     }
   });
-  Template.dino1.events({
+  Template.Triceratops.events({
     'click button': function (e) {
       // increment the counter when button is clicked
       Router.go('keypad');
     }
   });
-Template.dino2.events({
+Template.Stegosaurus.events({
     'click button': function (e) {
       // increment the counter when button is clicked
       Router.go('keypad');
     }
   });
-Template.dino3.events({
+Template.Apatosaurus.events({
     'click button': function (e) {
       // increment the counter when button is clicked
       Router.go('keypad');
     }
   });
-Template.dino4.events({
+Template.Archaeopteryx.events({
     'click button': function (e) {
       // increment the counter when button is clicked
       Router.go('keypad');
     }
   });
- Template.dino5.events({
+ Template.Gojirasaurus.events({
     'click button': function (e) {
       // increment the counter when button is clicked
       Router.go('keypad');
     }
   });
- Template.dino6.events({
+ Template.T_Rex.events({
     'click button': function (e) {
       // increment the counter when button is clicked
       Router.go('keypad');
     }
   });
- Template.dino7.events({
+ Template.Yoshi.events({
     'click button': function (e) {
       // increment the counter when button is clicked
       Router.go('keypad');
     }
   });
- Template.dino8.events({
+ Template.Thesaurus.events({
     'click button': function (e) {
       // increment the counter when button is clicked
       Router.go('keypad');
     }
   });
- Template.dino9.events({
+ Template.Philosoraptor.events({
     'click button': function (e) {
       // increment the counter when button is clicked
       Router.go('keypad');
     }
   });
-  Template.dino0.events({
+  Template.Barney.events({
     'click button': function (e) {
       // increment the counter when button is clicked
       Router.go('keypad');
